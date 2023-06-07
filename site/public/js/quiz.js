fe21b8873b9e3e1a0f8b355435c34c9791497856
kaikycruz <kaikycruz20@gmail.com>
2023-06-07 19:46:47 -0300

    07/06

---
+++ site/public/js/quiz.js
@@ -0,0 +1,13 @@
+var alternativaA = document.getElementById("alternativaA1");
+var alternativaB = document.getElementById("alternativaB1");
+var alternativaC = document.getElementById("alternativaC1");
+var alternativaD = document.getElementById("alternativaD1");
+
+var gabarito = ["Thousand Sunny"];
+
+var resposta = [];
+
+function clicou() {
+  console.log("cliclou na alternativa");
+  resposta.push();
+}
